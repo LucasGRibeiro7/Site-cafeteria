7b58defda7c8c6105957a00fd2d4b341c3f8c1b2
LucasRibeiro <lucas_2010sg@hotmail.com>
2025-02-25 17:41:13 -0300

    Projeto final site cafeteria

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/* Logica de programação
+    [x] Saber quando o usuario clicou no botão
+    [x] Preciso mudar o posicionamento do modal
+    [x] Fazer a máscara ficar visível
+    [ ] Quando clicar na máscara, fechar o modal
+*/
+
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara-modal")
+
+function mostrarmodal(){
+    modal.style.left = '50%'
+    mascara.style.visibility = 'visible'
+}
+
+function esconderModal(){
+    modal.style.left = '-30%'
+    mascara.style.visibility = 'hidden'
+}
